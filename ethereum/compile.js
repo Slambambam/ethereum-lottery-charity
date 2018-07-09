bad32f491734cacf18e46f17412795671734ad04
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2018-07-08 23:44:58 -0600

    Fix compilation for multiple contracts

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -7,20 +7,21 @@ const buildPath = path.resolve(__dirname, config.contracts.output);
 fs.removeSync(buildPath);
 
 let contractPath;
-let source;
+let sources = {};
 let output;
 
 for (let fileIndex in config.contracts.files) {
-    contractPath = path.resolve(__dirname, config.contracts.input, config.contracts.files[fileIndex]);
-    source = fs.readFileSync(contractPath, 'utf8');
-    output = solc.compile(source, 1).contracts;
+  contractPath = path.resolve(__dirname, config.contracts.input, config.contracts.files[fileIndex]);
+  sources[config.contracts.files[fileIndex]] = fs.readFileSync(contractPath, 'utf8');
+}
 
-    fs.ensureDirSync(buildPath);
+output = solc.compile({ sources }, 1).contracts;
 
-    for (let contract in output) {
-        fs.outputJsonSync(
-            path.resolve(buildPath, contract.replace(':', '') + '.json'),
-            output[contract]
-        );
-    }
+fs.ensureDirSync(buildPath);
+
+for (let contract in output) {
+  fs.outputJsonSync(
+    path.resolve(buildPath, contract.split(':')[1] + '.json'),
+    output[contract]
+  );
 }
